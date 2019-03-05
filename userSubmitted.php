b53619531555132fe0f46cd0c8830519b3e11b6e
ericamholland <46943367+ericamholland@users.noreply.github.com>
2019-03-05 15:20:35 -0800

    Add files via upload

--- userSubmitted.php
+++ userSubmitted.php
@@ -2,14 +2,14 @@
 include("mysql.php");
 $race = $_REQUEST["race"] ;
 $background = $_REQUEST["background"] ;
-$phys = $_REQUEST["physical[]"] ;
-$ment = $_REQUEST["mental[]"] ;
-$spirit = $_REQUEST["spiritual[]"] ;
-$maj_adv = $_REQUEST["maj_adv[]"] ;
-$min_adv = $_REQUEST["min_adv[]"] ;
-$maj_dis = $_REQUEST["maj_dis[]"] ;
-$min_dis = $_REQUEST["min_dis[]"] ;
-$traits = $_REQUEST["traits[]"] ;
+$phys = $_REQUEST["physical"] ;
+$ment = $_REQUEST["mental"] ;
+$spirit = $_REQUEST["spiritual"] ;
+$maj_adv = $_REQUEST["maj_adv"] ;
+$min_adv = $_REQUEST["min_adv"] ;
+$maj_dis = $_REQUEST["maj_dis"] ;
+$min_dis = $_REQUEST["min_dis"] ;
+$traits = $_REQUEST["traits"] ;
 
 var_dump($phys) ;
 
@@ -26,13 +26,13 @@ var_dump($phys) ;
     <p>Character will now be reviewed by Admin</p>
 
     <h2>Name:</h2>
-    <p><?=$name ?></p>
+    //<p><?=$name ?></p>
     <h2>Race:</h2>
-    <p><?=$race?></p>
+    <p><?=$race ?></p>
     <h2>Background:</h2>
-    <p><?=background?></p>
+    <p><?=background ?></p>
     <h2>Physical Skills:</h2>
     //<p><?=background?></p>
 
 </body>
-   </html>
+</html>
