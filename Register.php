456c020503b06c182e0554ad988559a42b5a05d9
h9uu <45111384+h9uu@users.noreply.github.com>
2019-03-07 14:15:41 -0800

    Update Register.php

--- Register.php
+++ Register.php
@@ -28,7 +28,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 /* store result */
                 mysqli_stmt_store_result($stmt);
                 
-                if(mysqli_stmt_num_rows($stmt) != 0){
+                if(mysqli_stmt_num_rows($stmt) == 0){
                     $username_err = "This username is already taken.";
                 } else{
                     $username = trim($_POST["username"]);
