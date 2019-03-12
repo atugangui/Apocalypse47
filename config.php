c337e4295cd476dc9a5650077e15940df527ecd0
h9uu <45111384+h9uu@users.noreply.github.com>
2019-03-12 13:31:17 -0700

    Update config.php

--- config.php
+++ config.php
@@ -8,14 +8,11 @@ define('DB_NAME', 'Apocalypse47');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
  
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-if($mysqli === false){
-    die("ERROR: Could not connect. " . $mysqli->connect_error);
-}
+
 ?>
