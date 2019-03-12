870dd0ad212998e191b24bb37ba67178a5fffb6c
amanda tugangui <45404063+atugangui@users.noreply.github.com>
2019-03-12 14:44:33 -0700

    Update sqlQueries.php

--- sqlQueries.php
+++ sqlQueries.php
@@ -1,5 +1,5 @@
 <?php
-require_once "config.php" ;
+include ("mysql.php") ;
 
 $name = $_REQUEST["name"] ;
 $pronouns = $_REQUEST["pronouns"] ;
@@ -13,23 +13,14 @@ $min_adv = $_REQUEST["min_adv"] ;
 $maj_dis = $_REQUEST["maj_dis"] ;
 $min_dis = $_REQUEST["min_dis"] ;
 $traits = $_REQUEST["traits"] ;
-echo $name ;
-
-     $query = "INSERT INTO character_table (char_name, race, background) VALUES(?,?,?)" ;
-     if($stmt = mysqli_prepare($link, $query)){
-        mysqli_stmt_bind_param($stmt, "sss", $param_name, $param_race, $param_bg) ;
-
-        $param_name = $name ;
-        $param_race = $race ;
-        $param_bg = $background ;
-
-        if(mysqli_stmt_execute($stmt)){
-            echo "good" ;
-            }
-            else{
-            echo "bad" ;
-            }
-     mysqli_stmt_close($stmt) ;
-
-   mysqli_close($link);
+ try{
+      $stmt = $conn->prepare("INSERT INTO character_table (char_name) VALUES ('Amanda')") ;
+      if($stmt->execute()){
+           echo "worked" ;
+      }
+ }
+catch(PDOException $e){
+     echo $e->getMessage() ;
+}
+      
 ?>
