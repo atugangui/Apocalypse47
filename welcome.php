06bc53494e97d7e99788f69e0a261985d90df3db
h9uu <45111384+h9uu@users.noreply.github.com>
2019-03-12 13:41:12 -0700

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,7 +1,6 @@
 <?php
-// Include config file
- require_once "config.php";
-
+// Include database connection
+require_once "mysql.php";
 // Initialize the session
 session_start();
  
@@ -30,35 +29,14 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <?php
  $name = htmlspecialchars($_SESSION["username"]);
         $sql = "SELECT char_id FROM character_table JOIN player_table ON player_table.player_id = character_table.player_id WHERE player_table.email = $name";
-        if($stmt = mysqli_prepare($link, $sql)){
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if (sizeof($rows)==0) {
+            ?><p>you do not have any characters</p><?php
+        } else {
             
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                /* store result */
-                mysqli_stmt_store_result($stmt);
-
-                $rows = mysqli_stmt_num_rows($stmt);
-                if ($rows == 0) {
-                    ?><p>You do not have any characters</p><?php
-                } else {
-                    if(mysqli_stmt_bind_result($stmt, $charIDS)){
-                        ?> <p>These are your character ids</p>
-                        <ul><?php
-                        while (mysqli_stmt_fetch($stmt)) { ?>
-                            <li><?= "%s", $charIDS ?></li>
-                        <?php
-                        }
-                        ?></ul><?php
-                    } else{
-                        echo "Something went wrong";
-                    }
-                }
-                
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
         }
-
     ?>
     <p>
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
