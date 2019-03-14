67ec4a0e8f77ef02f254ba785967a5d52e7a4028
amanda tugangui <45404063+atugangui@users.noreply.github.com>
2019-03-14 13:53:28 -0700

    Update changesSubmitted.php

--- changesSubmitted.php
+++ changesSubmitted.php
@@ -1,6 +1,20 @@
 <?php
 include("mysql.php");
-include("collectCharacterInput.php") ;
+
+$name = $_REQUEST["name"] ;
+$pronouns = $_REQUEST["pronouns"] ;
+$race = $_REQUEST["race"] ;
+$background = $_REQUEST["background"] ;
+$phys = $_REQUEST["physical"] ;
+$ment = $_REQUEST["mental"] ;
+$spirit = $_REQUEST["spiritual"] ;
+$maj_adv = $_REQUEST["maj_adv"] ;
+$min_adv = $_REQUEST["min_adv"] ;
+$maj_dis = $_REQUEST["maj_dis"] ;
+$min_dis = $_REQUEST["min_dis"] ;
+$traits = $_REQUEST["traits"] ;
+
+include("sqlQueries.php") ;
 ?>
 
 <html>
