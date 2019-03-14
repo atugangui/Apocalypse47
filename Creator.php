81f35a62c59690faedf118bbc3b006872cec1059
amanda tugangui <45404063+atugangui@users.noreply.github.com>
2019-03-14 13:11:08 -0700

    Update Creator.php

--- Creator.php
+++ Creator.php
@@ -1,14 +1,4 @@
 <?php
-
-// Initialize the session
-session_start();
- 
-// Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
-    exit;
-}
-$email = $_SESSION["username"] ;
 	
 $races = file_get_contents("race_types.csv") ;
 $races = explode("\r", $races) ;
