fc960aec450e202af89f2ee12a82d160dfad3ebc
h9uu <45111384+h9uu@users.noreply.github.com>
2019-03-14 13:44:33 -0700

    Update makePlayer.php

--- makePlayer.php
+++ makePlayer.php
@@ -3,6 +3,8 @@
 require_once "mysql.php";
 // Initialize the session
 session_start();
+
+$email = $_SESSION["username"];
  
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
@@ -10,9 +12,13 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     exit;
 }
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-
-}
+    $sql = "SELECT * FROM player_table WHERE player_table.email = :email";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(array(":email"=>$email));
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if (sizeof($rows)==0) {
+            header("location: welcome.php");
+        }
 ?>
 
 <!DOCTYPE html>
@@ -26,16 +32,20 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                     <div class="form-group">
                         <label>Name</label>
-                        <input type="text" name="Name" class="form-control" value="<?php echo $username; ?>">
-                        <span class="help-block"><?php echo $username_err; ?></span>
+                        <input type="text" name="Name" class="form-control" value="<?php echo $name; ?>">
                     </div>    
                     <div class="form-group">
                         <label>Pronouns</label>
-                        <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
+                        <input type="text" name="pronouns" class="form-control" value="<?php echo $pronouns; ?>">
                     </div>
                     <div class="form-group">
                         <input type="submit" class="btn btn-primary" value="Submit">
                         <input type="reset" class="btn btn-default" value="Reset">
+                        <?php
+                            $sql = "INSERT INTO player_table (email, NAME, pronouns) VALUES (?,?,?)";
+                            $stmt = $conn -> prepare($sql);
+                            $stmt -> execute([$email, $name, $pronouns]);
+                        ?>
                     </div>
                 </form>
             </div>
