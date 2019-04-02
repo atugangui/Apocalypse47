a9fa6a8b0e4fc309641149bbdd9c031491df214b
h9uu <45111384+h9uu@users.noreply.github.com>
2019-04-02 13:24:07 -0700

    Update creatorFlailing.js

--- creatorFlailing.js
+++ creatorFlailing.js
@@ -10,6 +10,7 @@ function dynamicdropdown(listindex)
             document.getElementById("subcategory").options[3]=new Option("Codeigniter","Codeigniter");
             document.getElementById("subcategory").options[4]=new Option("Joomla","Joomla");
             document.getElementById("subcategory").options[5]=new Option("Magento","Magento");
+            document.getElementById("subcategory").options[6]=new Option(races[1], races[1]);
             break;
 
         case "Java" :
