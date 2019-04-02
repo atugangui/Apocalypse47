cbcecd544c51c672bb7c0e9154d168818484090f
ShvetankPrakash <sprakash@lclark.edu>
2019-04-02 14:11:34 -0700

    modified changes submit file

--- changesSubmitted.php
+++ changesSubmitted.php
@@ -2,18 +2,18 @@
 include("mysql.php");
 
 //Gather user input information
-$name = $_REQUEST["name"] ;
-$pronouns = $_REQUEST["pronouns"] ;
-$race = $_REQUEST["race"] ;
-$background = $_REQUEST["background"] ;
-$phys = $_REQUEST["physical"] ;
-$ment = $_REQUEST["mental"] ;
-$spirit = $_REQUEST["spiritual"] ;
-$maj_adv = $_REQUEST["maj_adv"] ;
-$min_adv = $_REQUEST["min_adv"] ;
-$maj_dis = $_REQUEST["maj_dis"] ;
-$min_dis = $_REQUEST["min_dis"] ;
-$traits = $_REQUEST["traits"] ;
+$cname = $_REQUEST["name"] ;
+$cpronouns = $_REQUEST["pronouns"] ;
+$crace = $_REQUEST["race"] ;
+$cbackground = $_REQUEST["background"] ;
+$cphys = $_REQUEST["physical"] ;
+$cment = $_REQUEST["mental"] ;
+$cspirit = $_REQUEST["spiritual"] ;
+$cmaj_adv = $_REQUEST["maj_adv"] ;
+$cmin_adv = $_REQUEST["min_adv"] ;
+$cmaj_dis = $_REQUEST["maj_dis"] ;
+$cmin_dis = $_REQUEST["min_dis"] ;
+$ctraits = $_REQUEST["traits"] ;
 
 //Insert into database
 include("sqlQueries.php") ;
