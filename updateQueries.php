c6eb6f7bf381c46160819225bff49b7bda2f3ef5
ShvetankPrakash <sprakash@lclark.edu>
2019-04-04 15:17:21 -0700

    hallejujah

--- updateQueries.php
+++ updateQueries.php
@@ -7,48 +7,48 @@ $stmt->execute(array(":param_pid" => $pid, ":param_name" => $cname, ":param_race
 
 //Insert physical skills
 foreach($cphys as $p){
-    $stmt= $conn->prepare("INSERT INTO character_physical_skills (char_id, skill_name) VALUES(:param_cid, :param_phys)");
-    $stmt->execute(array(":param_cid"=>$charID, ":param_phys"=>$p)) ;
+    $stmt= $conn->prepare("UPDATE character_physical_skills SET char_id = :param_cchar_id, skill_name = :param_phys");
+    $stmt->execute(array(":param_cid"=>$cchar_id, ":param_phys"=>$p)) ;
 }
 
 //Insert mental skills
 foreach($cment as $m){
-    $stmt= $conn->prepare("INSERT INTO character_mental_skills (char_id, skill_name) VALUES(:param_cid, :param_ment)");
-    $stmt->execute(array(":param_cid"=>$charID, ":param_ment"=>$m)) ;
+    $stmt= $conn->prepare("UPDATE character_mental_skills SET char_id = :param_cchar_id, skill_name = :param_ment");
+    $stmt->execute(array(":param_cid"=>$ccharID, ":param_ment"=>$m)) ;
 }
 
 //Insert spiritual skills
 foreach($cspirit as $s){
-    $stmt= $conn->prepare("INSERT INTO character_spiritual_skills (char_id, skill_name) VALUES(:param_cid, :param_spirit)");
-    $stmt->execute(array(":param_cid"=>$charID, ":param_spirit"=>$s)) ;
+    $stmt= $conn->prepare("UPDATE character_mental_skills SET char_id = :param_cchar_id, skill_name = :param_spirit");
+    $stmt->execute(array(":param_cid"=>$ccharID, ":param_spirit"=>$s)) ;
 }
 
 //Insert major advantages
 foreach($cmaj_adv as $maja){
-    $stmt= $conn->prepare("INSERT INTO character_adv_and_disadv (char_id, TYPE, NAME) VALUES(:param_cid, :param_type, :param_maja)");
-    $stmt->execute(array(":param_cid"=>$charID, ":param_type"=>'major_advantage', ":param_maja"=>$maja)) ;
+    $stmt= $conn->prepare("UPDATE character_adv_and_disadv SET char_id = :param_cchar_id, TYPE = :param_type, NAME = :param_maja");
+    $stmt->execute(array(":param_cid"=>$ccharID, ":param_type"=>'major_advantage', ":param_maja"=>$maja)) ;
 }
 
 //Insert minor advantages
 foreach($cmin_adv as $mina){
-    $stmt= $conn->prepare("INSERT INTO character_adv_and_disadv (char_id, TYPE, NAME) VALUES(:param_cid, :param_type, :param_mina)");
-    $stmt->execute(array(":param_cid"=>$charID, ":param_type"=>'minor_advantage', ":param_mina"=>$mina)) ;
+    $stmt= $conn->prepare("UPDATE character_adv_and_disadv SET char_id = :param_cchar_id, TYPE = :param_type, NAME = :param_mina");
+    $stmt->execute(array(":param_cid"=>$ccharID, ":param_type"=>'minor_advantage', ":param_mina"=>$mina)) ;
 }
 
 //Insert major advantages
 foreach($cmaj_dis as $majd){
-    $stmt= $conn->prepare("INSERT INTO character_adv_and_disadv (char_id, TYPE, NAME) VALUES(:param_cid, :param_type, :param_majd)");
-    $stmt->execute(array(":param_cid"=>$charID, ":param_type"=>'major_disadvantage', ":param_majd"=>$majd)) ;
+    $stmt= $conn->prepare("UPDATE character_adv_and_disadv SET char_id = :param_cchar_id, TYPE = :param_type, NAME = :param_majd");
+    $stmt->execute(array(":param_cid"=>$ccharID, ":param_type"=>'major_disadvantage', ":param_majd"=>$majd)) ;
 }
 
 //Insert minor disadvantages
 foreach($cmin_dis as $mind){
-    $stmt= $conn->prepare("INSERT INTO character_adv_and_disadv (char_id, TYPE, NAME) VALUES(:param_cid, :param_type, :param_mind)");
-    $stmt->execute(array(":param_cid"=>$charID, ":param_type"=>'minor_disadvantage', ":param_mind"=>$mind)) ;
+    $stmt= $conn->prepare("UPDATE character_adv_and_disadv SET char_id = :param_cchar_id, TYPE = :param_type, NAME = :param_mind");
+    $stmt->execute(array(":param_cid"=>$ccharID, ":param_type"=>'minor_disadvantage', ":param_mind"=>$mind)) ;
 }
 
 //Insert traits
 foreach($ctraits as $trait){
-    $stmt= $conn->prepare("INSERT INTO character_adv_and_disadv (char_id, TYPE, NAME) VALUES(:param_cid, :param_type, :param_trait)");
-    $stmt->execute(array(":param_cid"=>$charID, ":param_type"=>'trait', ":param_trait"=>$trait)) ;
+    $stmt= $conn->prepare("UPDATE character_adv_and_disadv SET char_id = :param_cchar_id, TYPE = :param_type, NAME = :param_trait");
+    $stmt->execute(array(":param_cid"=>$ccharID, ":param_type"=>'trait', ":param_trait"=>$trait)) ;
 }
