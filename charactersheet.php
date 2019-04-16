a95feb9e357400db59927487bb8a3fb50f5725f6
amanda tugangui <45404063+atugangui@users.noreply.github.com>
2019-04-16 14:03:44 -0700

    Update charactersheet.php

--- charactersheet.php
+++ charactersheet.php
@@ -173,6 +173,9 @@ include ("getCharacterOptions.php") ;
       <?php } ?>
 
 <input type="submit" name="submit"/>
+	
+	 <script src="https://code.jquery.com/jquery-3.4.0.min.js" integrity="sha256-BJeo0qm959uMBGb65z40ejJYGSgR7REI4+CW1fNKwOg=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 
 <script>
 
