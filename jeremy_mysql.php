e161d09d4bb419cc1a76a1aacf3482c65b67edbd
amanda tugangui <45404063+atugangui@users.noreply.github.com>
2019-04-16 15:29:27 -0700

    Create jeremy_mysql.php

---
+++ jeremy_mysql.php
@@ -0,0 +1,38 @@
+<?php
+/*
+This file contains the class for the mysql connection and all its functions/interactions
+*/
+class mysqlFunctions{
+  function __construct(){
+    include("config.php");
+    $connect="mysql:host=$server;dbname=$database";
+    try {
+      $db = new PDO($connect, $username, $password);
+    }
+    catch(PDOException $e) {
+      echo $e->getMessage();
+    }
+    $this->db=$db;
+  }
+
+
+  function updateName($id, $name){
+    $db=$this->db;
+    try {
+      $stmt = $db->prepare("update users set name=:name where id=:id");
+      $stmt->execute(array(":id"=>$id, ":name"=>$name));
+      $count = $stmt->rowCount();
+      if($count =='0'){
+          return false;
+      }
+      else{
+          return true;
+      }
+      //var_dump($rows);
+    }
+    catch (Exception $e) {
+      echo $e;
+    }
+  }
+}
+ ?>
