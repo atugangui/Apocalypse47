24a5926b5b0418b9084f26d92142e3e5fab9dcd1
amanda tugangui <45404063+atugangui@users.noreply.github.com>
2019-04-16 15:18:01 -0700

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -11,4 +11,17 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 } 
 $fx=$_POST["fx"];
 
+if($fx == "updateName"){
+   updateName() ;
+}
+
+function updateName(){
+  $name=$_POST["name"];
+  $id=$_POST["char_id"];
+  $stmt = $conn->prepare("UPDATE character_table SET char_name = :param_name WHERE char_id = :param_cid");
+  if($stmt->execute(array(":param_name" => $name, ":param_cid" => $id))){
+    echo "success";
+  }
+  else{echo "fail";}
+}
 ?>
