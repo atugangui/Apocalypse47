47e8a58c7e061e00e393bced91f220ccd453bc44
h9uu <45111384+h9uu@users.noreply.github.com>
2019-04-18 13:06:16 -0700

    Update changesSubmitted.php

--- changesSubmitted.php
+++ changesSubmitted.php
@@ -1,6 +1,5 @@
 <?php
 include("mysql.php");
-
 //Gather user input information
 $cchar_id = $_REQUEST["char_id"];
 $cname = $_REQUEST["name"] ;
@@ -15,12 +14,10 @@ $cmin_adv = $_REQUEST["min_adv"] ;
 $cmaj_dis = $_REQUEST["maj_dis"] ;
 $cmin_dis = $_REQUEST["min_dis"] ;
 $ctraits = $_REQUEST["traits"] ;
-
 $physical_cost = 0;
 $mental_cost = 0;
 $spiritual_cost = 0;
 $advantage_disadvantage_weight = 0;
-
 for ($i=0; $i < sizeof($cphys); $i++) {
     $temp = explode(",",$cphys[$i]);
     for ($j=0; $j < sizeof($temp); $j++) {
@@ -28,7 +25,6 @@ for ($i=0; $i < sizeof($cphys); $i++) {
         $physical_cost = $physical_cost + $physical_skills[$i][1];
     }
 }
-
 for ($i=0; $i < sizeof($cment); $i++) {
     $temp = explode(",",$cment[$i]);
     for ($j=0; $j < sizeof($temp); $j++) {
@@ -36,7 +32,6 @@ for ($i=0; $i < sizeof($cment); $i++) {
         $mental_cost = $mental_cost + $mental_skills[$i][1];
     }
 }
-
 for ($i=0; $i < sizeof($cspirit); $i++) {
     $temp = explode(",",$cspirit[$i]);
     for ($j=0; $j < sizeof($temp); $j++) {
@@ -44,19 +39,16 @@ for ($i=0; $i < sizeof($cspirit); $i++) {
         $spiritual_cost = $spiritual_cost + $spiritual_skills[$i][1];
     }
 }
-
 for ($i=0; $i < sizeof($cmaj_adv); $i++) {
     $temp = explode(",",$cmaj_adv[$i]);
     for ($j=0; $j < sizeof($temp); $j++) {
         $major_advantages[$i][$j] = $temp[$j];
         print($major_advantages[$i][$j]);
-        $advantage_disadvantage_weight = $advantage_disadvantage_weight + $major_advantages[$i][2];
+        $advantage_disadvantage_weight = $advantage_disadvantage_weight + $major_advantages[$i][1];
     }
 }
 print($advantage_disadvantage_weight); //// THIS IS WHATS NOT WORKING
-
-
-if($physical_cost + $mental_cost + spiritual_cost > 50){
+if($physical_cost + $mental_cost + $spiritual_cost > 50){
 	print("ya fucked up. You have more than 50 total points. Do it better this time.");
 }
 elseif($physical_cost > 10 && $mental_cost > 10 && $spiritual_cost > 10){
@@ -68,12 +60,7 @@ elseif($physical_cost > 20 || $mental_cost > 20 || $spiritual_cost > 20){
 else {
 	//Insert into database
 	include("sqlQueries.php") ;
-
 	include("charactersheet.php") ;
 }
-
-
 print($phys_test[0][0]);
-
-
 ?>
