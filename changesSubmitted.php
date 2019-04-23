cf38233203555903d912769b8269af98044a6044
h9uu <45111384+h9uu@users.noreply.github.com>
2019-04-23 13:40:55 -0700

    Update changesSubmitted.php

--- changesSubmitted.php
+++ changesSubmitted.php
@@ -95,7 +95,7 @@ if ($errors==0) {
     include("charactersheet.php") ;
 } else {
     ?><script language="javascript" type="text/javascript">
-        alert("You messed up " + 7);// + <?=$err[0] ?> );
+        alert("You messed up " + <?=$err[0] ?>);// + <?=$err[0] ?> );
             // <?php foreach ($errors as $error) {
             //     ?> <?= $error ?> +
             // <?php } ?>
