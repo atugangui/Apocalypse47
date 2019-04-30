b9372d09d7c92fee923610d6d891890c16fc1aac
amanda tugangui <45404063+atugangui@users.noreply.github.com>
2019-04-30 14:53:04 -0700

    Add files via upload

--- welcome.php
+++ welcome.php
@@ -18,8 +18,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <title>Home</title>
     <!-- Compiled and minified CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <!-- Custom styles for this template -->
-    <link href="pricing.css" rel="stylesheet">
+
 </head>
 <body>
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
