8a2cc210a235b27063b23f8fe186d00d1d83cd93
ericamholland <46943367+ericamholland@users.noreply.github.com>
2019-04-30 14:34:27 -0700

    Add files via upload

--- welcome.php
+++ welcome.php
@@ -55,7 +55,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 	            	<div class="col-4">
 	            		<div class="card">
 	            			<div class="card-body">
-	            				<h1 class="card-title"><?php echo $rows[0]['char_name'] ?></h1>
+	            				<h1 class="card-title">hello!</h1>
 	            			</div>
 	            		</div>
 	            	</div>
