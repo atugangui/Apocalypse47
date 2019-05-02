fe66618453eccb9524ac004380ed135c8104caea
amanda tugangui <45404063+atugangui@users.noreply.github.com>
2019-05-02 14:12:43 -0700

    Add files via upload

--- charactersheet.php
+++ charactersheet.php
@@ -93,6 +93,9 @@ $traitSelect = "<select multiple='multiple' id='selectedTrait' name='trait'>" ;
 <head>
     <!-- Compiled and minified CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Custom CSS for charactersheet.php -->
+    <link href="charactersheet.css" rel="stylesheet">
   <meta charset="utf-8" />
 </head>
 <body>
